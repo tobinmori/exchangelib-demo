d94120536d45918623bcea74d964af9f554e21a8
Tobin <tobinmori@gmail.com>
2016-10-21 15:33:22 -0400

    init commit of demo

---
+++ get_email.py
@@ -0,0 +1,82 @@
+from exchangelib import DELEGATE, IMPERSONATION, Account, Credentials, \
+    EWSDateTime, EWSTimeZone, Configuration, NTLM, CalendarItem, Q
+
+credentials = Credentials(username='', password='')
+
+account = Account(primary_smtp_address='', credentials=credentials,
+                  autodiscover=True, access_type=DELEGATE)
+
+WHITELIST_FOLDERS = ['','']
+
+def print_account_folders():
+    """
+    Mainly, for debug purposes so that you can see you folder hierarchy and
+    whether there are any children.
+
+    INTERESTINGLY, Inbox and others have no subfolders or subitems.
+
+    :return:
+    """
+    for k,v in account.folders.items():
+        print(k,v)
+
+def print_all_root_folders():
+    """
+    Prints ALL folders under Root.
+
+    :return:
+    """
+    print(account.root.get_folders())
+
+def get_all_root_folders():
+    """
+    An unelegant way to get a list of the Root folder
+    (by getting the ONLY folder that has children)
+    """
+    for folder in account.folders.keys():
+        if len(account.folders[folder]) > 0:
+            return account.folders[folder]
+    return None
+
+def get_root_folders_by_name():
+    """
+    Returns a list of the 1st generation of folders under root.
+    """
+    return [str(folder) for folder in get_all_root_folders()]
+
+def print_emails(folder):
+    if (folder.all().count() > 1):
+        for m in folder.all():
+            print(m)
+    else:
+        item = folder.get()
+        print(item)
+
+def extract_folder(folder):
+    """
+    Returns a list of emails, given a folder
+    """
+    emails = []
+    if (folder.all().count() > 1):
+        for m in folder.all():
+            emails.append(m)
+    else:
+        item = folder.get()
+        emails.append(item)
+    return emails
+
+def get_emails():
+    """
+    Returns a list of emails, in the WHITELISTED list.
+    :return:
+    """
+    items = []
+    for folder in get_all_root_folders():
+        if str(folder) in WHITELIST_FOLDERS:
+            items.extend(extract_folder(folder))
+        else: continue
+    return items
+
+print(len(get_emails()))
+print(get_emails()[80])
+
